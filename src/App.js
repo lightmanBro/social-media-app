0936368c16d7d46123292c5f4f56a6aaf9cc2df7
lightmanBro <omotosodavidromeo@gmail.com>
2022-10-27 13:05:59 +0100

    Building the Login Page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import Login from "./components/login/Login";
 import { Home } from "./pages/home/Home";
 import Profile from "./pages/profile/Profile";
 // import {Person} from '@mui/icons-material'
 function App() {
   return (
     // <Person/>
-    <Profile/>
+    <Login/>
   );
 }
 
